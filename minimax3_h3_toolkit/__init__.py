0ca1f9c81915166ada43845901141038edb40445
jayseanbrambila-del666 <jayseanbrambila@gmail.com>
2026-08-30 00:21:56 +0800

    Add minimax3_h3_toolkit/__init__.py

---
+++ minimax3_h3_toolkit/__init__.py
@@ -0,0 +1,9 @@
+from .core import build_prompt, build_run_summary, build_workflow_advice, validate_prompt
+
+__all__ = [
+    "build_prompt",
+    "build_run_summary",
+    "build_workflow_advice",
+    "validate_prompt",
+]
+
